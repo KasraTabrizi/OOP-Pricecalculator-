7d1fdd1657323249529310a38aaca9b4f6a0d628
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-16 12:39:14 +0100

    created function createTable and calculatePrice

--- model/model.php
+++ model/model.php
@@ -1,5 +1,13 @@
 <?php
+    //CREATE TABLE AND SEND TO THE VIEW
+    function createTable(){
 
+    }
+
+    //CALCULATE THE PRICE OF THE PRODUCT FOR EACH DISCOUNT
+    function calculatePrice(){
+        
+    }
 
 
 ?>
