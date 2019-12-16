5b06d4c0f6f89c8fb64faf36fc0bad8323df513f
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-16 12:35:04 +0100

    created class Product

--- model/classes.php
+++ model/classes.php
@@ -27,7 +27,33 @@ class Customer{
 }
 
 class Product{
-    
+    private $id;
+    private $name;
+    private $description;
+    private $price;
+
+    public function __construct($id, $name, $description, $price){
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+
+    public getId(){
+        return $this->id; 
+    }
+
+    public getName(){
+        return $this->name;
+    }
+
+    public getDescription(){
+        return $this->description;
+    }
+
+    public getPrice(){
+        return $this->price;
+    }
 }
 
 ?>
