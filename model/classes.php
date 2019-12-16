8544ecff71ecb1160313e482e244070188bcdf7d
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-16 14:39:59 +0100

    created class Department and class Company

--- model/classes.php
+++ model/classes.php
@@ -26,6 +26,7 @@ class Customer{
 
 }
 
+//CLASS PRODUCT
 class Product{
     private $id;
     private $name;
@@ -53,6 +54,86 @@ class Product{
 
     public getPrice(){
         return $this->price;
+    }
+}
+
+//CLASS DEPARTEMENT
+class Departement{
+    private $id;
+    private $name;
+    private $description;
+    private $discountType;
+    private $discountValue;
+    private $groupId;   
+
+    public function __construct($id, $name, $description, $discountType, $discountValue, $groupId){
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->discountType = $discountType;
+        $this->discountValue = $discountValue;
+        $this->groupId = $groupId;
+    }
+
+    public getId(){
+        return $this->id; 
+    }
+
+    public getName(){
+        return $this->name;
+    }
+
+    public getDescription(){
+        return $this->description;
+    }
+
+    public getDiscountType(){
+        return $this->discountType;
+    }
+
+    public getDiscountValue(){
+        return $this->discountValue;
+    }
+
+    public getGroupId(){
+        return $this->groupId;
+    }
+}
+
+//CLASS COMPANY
+class Company{
+    private $id;
+    private $name;
+    private $description;
+    private $discountType;
+    private $discountValue; 
+
+    public function __construct($id, $name, $description, $discountType, $discountValue){
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->discountType = $discountType;
+        $this->discountValue = $discountValue;
+    }
+
+    public getId(){
+        return $this->id; 
+    }
+
+    public getName(){
+        return $this->name;
+    }
+
+    public getDescription(){
+        return $this->description;
+    }
+
+    public getDiscountType(){
+        return $this->discountType;
+    }
+
+    public getDiscountValue(){
+        return $this->discountValue;
     }
 }
 
