0361cbff25920471392373f6600f93b5bcd60bae
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-19 15:57:00 +0100

    created login.php and registration.php

---
+++ view/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Price Calculator</title>
+    <style>
+        <?php include '../styles/css/styles.css'; ?>
+    </style>
+</head>
+
+<?php 
+    //require '../controller/controller.php'; 
+?>
+
+<body>
+    <div id="wrapper">
+        <form action="" method="POST">
+            <input type="text" name="" id="">
+            <input type="text" name="" id="">
+            <input type="submit" value="Login" name="login">
+            <input type="submit" value="Create Account" name="registration">
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ view/registration.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Price Calculator</title>
+    <style>
+        <?php include '../styles/css/styles.css'; ?>
+    </style>
+</head>
+
+<?php 
+    //require '../controller/controller.php'; 
+?>
+
+<body>
+    <div id="wrapper">
+        <form action="" method="POST">
+            <!-- Customer Dropdown -->
+            <select class="dropdown" name="customer_dropdown"> 
+                <?php 
+                    foreach($customers as $customer){
+                        echo "<option value=".$customer->getId().">".$customer->getName()."</option>";
+                    }
+                ?>
+            </select>
+            <!-- Products Dropdown -->
+            <select class="dropdown" name="product_dropdown"> 
+                <?php 
+                    foreach($products as $product){
+                        echo "<option value=".$product->getId().">".$product->getName()."</option>";
+                    }
+                ?>
+            </select>
+            <!-- INPUT TEXT FOR AMOUNT -->
+            <input type="text" name="amount" id="" placeholder="amount">
+            <!-- Calculate Button -->
+            <input type="submit" value="calculate" name="calculate">
+        </form>
+        <!-- Price Table -->
+        <table id="customers">
+        <?php 
+            if(isset($retrievedData)){
+                createTable($retrievedData, $companies);
+            }  
+        ?>
+        </table>
+    </div>
+</body>
+
+</html>
